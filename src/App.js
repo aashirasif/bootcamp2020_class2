9228bededeaabfa56fb5f147f8f799240f8bbb4d
aashirasif <71927384+aashirasif@users.noreply.github.com>
2020-10-01 18:01:53 +0500

    Checking

--- src/App.js
+++ src/App.js
@@ -6,11 +6,7 @@ function App() {
     <div class="Main">
       <div class="Main2">
         <ul>
-          <li>Aashir</li>
-          <li>Rashode main kaun tha??</li>
-          <li>OO bhai maro mujhe maroo </li>
-          <li>EMpty</li>
-          <li>Abba harmonium bajaty thy</li>
+          <li>Successfully Built</li>
         </ul>
       </div>
     </div>
