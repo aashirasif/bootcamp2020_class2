1e1ae980a47763161d2cb86751e4bd3ff7983e14
aashirasif <71927384+aashirasif@users.noreply.github.com>
2020-10-07 16:03:35 +0500

    Update

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+  let [count,setCount] = useState(0);
   return (
-    <div class="Main">
-      <div class="Main2">
-        <ul>
-          <li>Successfully Built</li>
-        </ul>
-      </div>
-    </div>
+   <div>
+     <h2>Count = {count}</h2>
+     <br/>
+     <button onClick={()=> setCount(++count)}>+</button>
+     <button onClick={()=> setCount(--count)}>-</button>
+   </div>
   );
 }
 
